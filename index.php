057dfdb151d498743f21ca3a669906cbc5441489
Arnav Goyal <goyalagf@gmail.com>
2023-12-28 19:32:52 +0530

    add processCSV function

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>WhatsApp Messaging System</title>
+    <title>WhatsApp Bulk Messaging System</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 
 <body>
-    
+
     <div class="container mt-5">
         <div class="card">
             <h1 class="text-center mb-4">WhatsApp Bulk Messaging System</h1>
@@ -23,7 +23,7 @@
                     <input type="file" class="form-control-file" id="csvFileInput" name="csvFileInput" accept=".csv">
                 </div>
 
-                <!-- Add a hidden input field to store phone numbers -->
+                <!-- Adding a hidden input field to store phone numbers -->
                 <input type="hidden" name="phoneNumbers" id="phoneNumbersInput" value="">
 
                 <button type="button" class="btn btn-success" onclick="processCSV()">Process CSV</button>
@@ -36,6 +36,25 @@
     <footer>
         <p>&copy; 2023 Code Sprinters. All rights reserved.</p>
     </footer>
+
+    <script>
+        function processCSV() {
+            // Using AJAX to call the PHP script
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    console.log(xhr.responseText);
+                    // Setting the phone numbers in the hidden input field
+                    document.getElementById('phoneNumbersInput').value = xhr.responseText;
+                }
+            };
+
+            var formData = new FormData();
+            formData.append('csvFileInput', document.getElementById('csvFileInput').files[0]);
+            xhr.open('POST', 'process_csv.php', true);
+            xhr.send(formData);
+        }
+    </script>
     
 </body>
 </html>
