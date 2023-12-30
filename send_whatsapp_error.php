3c7964e86871e97c4796d9be52327332c6a84d11
Arnav Goyal <goyalagf@gmail.com>
2023-12-30 08:58:40 +0530

    Add Confirmation Page, Add Styling, Modified the Existing Styling of Main Page

---
+++ send_whatsapp_error.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error Page</title>
+    <link rel="stylesheet" href="confirmcss.css">
+</head>
+<body>
+
+    <div id="container">
+        <div id="error-box">
+          <div class="dot"></div>
+          <div class="dot two"></div>
+          <div class="face2">
+            <div class="eye"></div>
+            <div class="eye right"></div>
+            <div class="mouth sad"></div>
+          </div>
+          <div class="shadow move"></div>
+          <div class="message"><h1 class="alert">Error!</h1><p><b>Oops! Something Went Wrong.</b></p></div>
+          <button class="button-box" onclick="location.href='index.php'"><h1 class="red"><b>try again</b></h1></button>
+        </div>
+      </div>
+
+</body>
+</html>
---
+++ send_whatsapp_success.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Success Page</title>
+    <link rel="stylesheet" href="confirmcss.css">
+</head>
+<body>
+
+    <div id="container">
+        <div id="success-box">
+          <div class="dot"></div>
+          <div class="dot two"></div>
+          <div class="face">
+            <div class="eye"></div>
+            <div class="eye right"></div>
+            <div class="mouth happy"></div>
+          </div>
+          <div class="shadow scale"></div>
+          <div class="message"><h1 class="alert">Success!</h1><p><b>YAY! Message Sent Successfully.</b></p></div>
+          <button class="button-box" onclick="location.href='index.php'"><h1 class="green"><b>continue</b></h1></button>
+        </div>
+      </div>
+
+</body>
+</html>
