8f3910bef0bfb1a2b2e37c24311d5978fe2e3cae
Arnav Goyal <goyalagf@gmail.com>
2023-12-30 09:59:56 +0530

    Add Python API

---
+++ project.py
@@ -0,0 +1,34 @@
+# import pywhatkit
+# pywhatkit.sendwhatmsg('+91XXXXXXXXXX', 'bhagwan bharose', 23, 7)
+
+import pandas as pd
+import pywhatkit
+from datetime import datetime, timedelta
+
+# Function to send messages
+def send_whatsapp_messages(phone_number, message, hour, minute):
+    try:
+        pywhatkit.sendwhatmsg(phone_number, message, hour, minute)
+        print(f"Message sent to {phone_number} successfully!")
+    except Exception as e:
+        print(f"Error sending message to {phone_number}: {str(e)}")
+
+# Read phone numbers from CSV file
+csv_file_path = 'test.csv'  # Change this to the path of your CSV file
+df = pd.read_csv(csv_file_path)
+
+# Current time
+now = datetime.now()
+
+# Iterate over rows in the DataFrame
+for index, row in df.iterrows():
+    phone_number = str(row['Phone'])
+    message = 'Your custom message here'  # Replace with your desired message
+    hour = now.hour
+    minute = now.minute + 2  # Send the message 2 minutes from now
+
+    # Call the function to send the message
+    send_whatsapp_messages(phone_number, message, hour, minute)
+
+    # Increment the minute for the next message
+    now += timedelta(minutes=1)
